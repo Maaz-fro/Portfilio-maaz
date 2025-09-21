8aa889f913f98185bfb55964b9b2306575549997
Maaz-fro <maaaz4452@gmail.com>
2025-09-22 04:04:00 +0500

    Fix

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base:"Portfilio-maaz",
+  base:"/Portfilio-maaz/",
   plugins: [react(),
      tailwindcss(),
   ],
